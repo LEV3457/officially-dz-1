9672831fa4fdc5d1c8dfb1db3fc1aa65ba06249e
unknown <george2748@mail.ru>
2025-01-10 21:23:45 +0300

    b

---
+++ task_dz/task4.js
@@ -0,0 +1,8 @@
+let a = 8;
+if (0 <= a <= 9) {
+    console.log("правильно"); 
+} else if (0 > a) {
+    console.log("плохо");
+} else {
+    console.log("плохо");
+}
---
+++ task_dz/task5.js
@@ -0,0 +1,9 @@
+const prompt = require('prompt-sync')();
+let x = prompt('Введите число:');
+
+if ( 0 >= x && x<=10 (x % 2 == 0)) {
+    console.log('Число правилно');
+} 
+ else {
+    console.log("грустно");
+}
