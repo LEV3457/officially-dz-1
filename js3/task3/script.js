5427e88c9e3288b1418b4ab3254f1c142a1816b4
LEV3457 <lukyanovivan83@gmail.com>
2025-02-02 21:04:24 +0300

    tasks are done

--- js3/task3/script.js
+++ js3/task3/script.js
@@ -2,12 +2,14 @@ document.getElementById('Button').addEventListener('click', function() {
     // const name = prompt("Введите свое имя: ")
     const name = document.getElementById('nameInput').value;
     const message = document.getElementById('message'); 
-    let i = document.getElementById('nameInput');
-
     
-        
-        for (let i = name; i > 0; i -= 2) 
-            alert(i);  
+    for (let i = 1; i <= name ; i++) {
+        let r = name % i;
+            if (r == 0){
+                str += i + ', '
+                }
+      
     }
-  
+
+
 });
